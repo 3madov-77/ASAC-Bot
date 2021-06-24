afc3ed8b9b59801a99c4f84742cca8fc901f3912
AbdulhakimZatar <hakimzatar@gmail.com>
2021-06-25 00:03:27 +0300

    handle commands

--- index.js
+++ index.js
@@ -3,11 +3,17 @@
 const Discord = require('discord.js');
 require('dotenv').config();
 
+const command = require('./models/command');
+
 const token = process.env.TOKEN;
 const client = new Discord.Client();
 
-client.on('ready',()=>{
+client.on('ready', () => {
   console.log('ready');
+
+  command(client, 'ping', (msg) => {
+    msg.channel.send('Pong');
+  });
 });
 
 client.login(token);
