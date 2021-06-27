d4288771b7fe063bcc9c092c4b5320d9cba5a6d6
Abdulhakimzatar <hakimzatar@gmail.com>
2021-06-27 15:07:48 +0300

    fix

--- models/tickets/create.js
+++ models/tickets/create.js
@@ -89,6 +89,7 @@ module.exports = async (client) => {
         }
         let guild = await client.guilds.fetch(GUILD);
         // await createChannel(guild, `📗test📗`, '856836553623863307');
+        console.log(checkTicket(button.clicker.user.id));
         if (checkTicket(button.clicker.user.id)) {
           console.log(nickname, 'spam ticket');
           const embed = new Discord.MessageEmbed().setDescription(`You have an opened ticket.`).setTitle('ASAC Tickets System').setColor('#ffc107');
